777028e02e359874e598675a43286b31f586a5c2
Xaralampos Makridhs <makridhs.xaralampos@gmail.com>
2026-05-08 19:03:45 +0300

    implementation index.php for redirecting

---
+++ public/index.php
@@ -0,0 +1,24 @@
+<?php
+    require_once __DIR__.'/../bootstrap.php';
+    require_once __DIR__.'/../LinkServices/ShortLinkService.php';
+
+    $shortLinkService=new ShortLinkService($conn);
+
+    $path=parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+    $shortCode=trim($path,'/');
+
+    if($shortCode===''){
+        echo "URL Shortener Home";
+        exit;
+    }
+
+    $link=$shortLinkService->findByCode($shortCode);
+
+    if(!$link){
+        http_response_code(404);
+        echo "Short link not found";
+        exit;
+    }
+
+    header('Location: '.$link['original_url'],true,302);
+    exit;
