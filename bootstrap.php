77f8f0afed6de9e318b21b3480ed4c844d8ce24d
Xaralampos Makridhs <makridhs.xaralampos@gmail.com>
2026-05-09 21:44:41 +0300

    update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__. '/Config/Database.php';
 
 if (!file_exists(__DIR__ . '/.env')) {
     throw new Exception('.env file not found');
@@ -10,11 +9,19 @@ if (!file_exists(__DIR__ . '/.env')) {
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+require_once __DIR__ . '/Config/Database.php';
+
 // session security
 ini_set('session.cookie_httponly', 1);
 ini_set('session.use_strict_mode', 1);
 
-
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
+}
+
+$database = new Database();
+$conn = $database->getConnection();
+
+if (!$conn) {
+    die('Database connection failed.');
 }
