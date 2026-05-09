23484bfa763e9ea669bc9990fc9b6aeb1336183b
Xaralampos Makridhs <makridhs.xaralampos@gmail.com>
2026-05-09 22:17:26 +0300

    update and debug login

--- public/login.php
+++ public/login.php
@@ -1,29 +1,31 @@
 <?php
-    require_once __DIR__.'/../bootstrap.php';
-    require_once __DIR__.'/../Services/AuthService.php';
 
-    $auth=new AuthService($conn);
-    if($auth->check()){
-       header('Location: dashboard.php');
-       exit;
+require_once __DIR__ . '/../bootstrap.php';
+require_once __DIR__ . '/../Services/AuthService.php';
+
+$auth = new AuthService($conn);
+
+if ($auth->check()) {
+    header('Location: dashboard.php');
+    exit;
+}
+
+$error = null;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $email = $_POST['email'] ?? '';
+    $password = $_POST['password'] ?? '';
+
+    $loggedIn = $auth->login($email, $password);
+
+    if ($loggedIn) {
+        header('Location: dashboard.php');
+        exit;
     }
 
-    $error=null;
-
-    if($_SERVER['REQUEST_METHOD']==='POST') {
-        $email = $_POST['email'];
-        $password = $_POST['password'] ?? '';
-
-        $loggedIn = $auth->login($email, $password);
-
-        if ($loggedIn) {
-            header('Location: dashboard.php');
-            exit;
-        } else {
-            $error = "Invalid email or password";
-        }
-    }
-
+    $error = 'Invalid email or password.';
+}
 ?>
 
 <!DOCTYPE html>
@@ -33,30 +35,51 @@
     <title>Login</title>
 </head>
 <body>
-    <h1>Log in</h1>
-    <?php if($error):?>
-        <p style="color: red;">
-            <?=htmlspecialchars($error);?>
-        </p>
-    <?php endif;?>
 
-    <form method="post">
-        <div>
-            <label>Email</label>
-            <input type="email" name="email" required>
-        </div>
-        <br>
-        <div>
-            <label>Password</label>
-            <input type="password" name="password" required>
-        </div>
-        <br>
-        <button type="submit">Login</button>
-    </form>
-    <br>
-    <p>
-        Don't have an account?
-        <a href="register.php">Register</a>
+<h1>Log in</h1>
+
+<?php if ($error): ?>
+    <p style="color: red;">
+        <?= htmlspecialchars($error) ?>
     </p>
+<?php endif; ?>
+
+<form method="POST" action="login.php">
+
+    <div>
+        <label>Email</label><br>
+        <input
+                type="email"
+                name="email"
+                required
+        >
+    </div>
+
+    <br>
+
+    <div>
+        <label>Password</label><br>
+        <input
+                type="password"
+                name="password"
+                required
+        >
+    </div>
+
+    <br>
+
+    <button type="submit">
+        Login
+    </button>
+
+</form>
+
+<br>
+
+<p>
+    Don't have an account?
+    <a href="register.php">Register</a>
+</p>
+
 </body>
 </html>
